3d7606549f6f5e69269758188713512e0c21d022
Mikhail Gaydamakha <mikhail.gaydamakha@etu.unistra.fr>
2020-05-19 16:24:54 -0400

    Add script for simple metrics

---
+++ scripts/icps_collect_uni_metrics.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+# In this script we suppose that "make" and "make certs" are already done
+
+DIRNAME="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+SRC_DIR="$DIRNAME/../"
+SERVER_FILE="$SRC_DIR/server.txt"
+MACHINES_FILE="$SRC_DIR/machines.txt"
+FIXTURES_DIR="$SRC_DIR/fixtures"
+RESULTS_DIR="$SRC_DIR/results"
+TXT_DIR="$SRC_DIR/txt"
+NB_FILES=20
+SERVER_PORT=5000
+USER=$(id -un)
+MAX_CHUNK_SIZE=2097152
+NB_ITERS=3
+#Try to repeat a measure 3 times before abandon
+MAX_TRIES=3
+BIN=$HOME/go/bin/ter-grpc
+
+if [[ ! -f "$MACHINES_FILE" ]]; then
+    echo "File $MACHINES_FILE does not exists"
+    exit 1
+fi
+
+mapfile -t MACHINES <$MACHINES_FILE
+
+mkdir -p $RESULTS_DIR
+mkdir -p $TXT_DIR
+
+RESULTS_FN=$RESULTS_DIR/results-nbwrks-0.txt
+# Clear the file if it exists
+>$RESULTS_FN
+#Deploy the server
+echo "Deploying the server..."
+#Clear the file
+>$SERVER_FILE
+#Take the first machine in the list
+SERVER_AD=${MACHINES[0]}
+SERVER_IP=""
+until [[ -n "$SERVER_IP" ]]; do
+    SERVER_IP=$(ssh $SERVER_AD 'echo $SSH_CONNECTION' | cut -d ' ' -f3)
+    sleep 1
+done
+echo "Deploying the server in address $SERVER_IP:$SERVER_PORT ($SERVER_AD)..."
+CODE=1
+until [[ CODE -eq 0 ]]; do
+    ssh $SERVER_AD $DIRNAME/server_remote_launch.sh -p $SERVER_PORT -b $BIN -c 4096 -a ""
+    CODE=$?
+    sleep 1
+done
+
+echo "$SERVER_AD $SERVER_IP" >>$SERVER_FILE
+echo "Success!"
+#Fetch server IP
+SERVER_IP=$(cat $SERVER_FILE | cut -d ' ' -f2)
+#The initial client chunk size value
+CLIENT_CHUNK_SIZE=16
+while [[ $CLIENT_CHUNK_SIZE -le $MAX_CHUNK_SIZE ]]; do
+    for FILENAME in $FIXTURES_DIR/*.pdf; do
+        echo "Launching client with chunk_size:$CLIENT_CHUNK_SIZE"
+        #Make a measure 3 times to calculate an average value
+        ITER=0
+        TIME=0
+        NB_SUCCESS=0
+        while [[ $ITER -lt $NB_ITERS ]]; do
+            FILENAME_BASE=$(basename -- "$FILENAME")
+            mkdir -p $TXT_DIR
+            CODE=1
+            TRY=1
+            ITER_TIME=0
+            #repeat until successfull or number of tries is achieved
+            until [[ $CODE -eq 0 || $TRY -gt $MAX_TRIES ]]; do
+                ITER_TIME=$($BIN pdftotext --compress=true --root-certificate $SRC_DIR/certs/localhost.cert \
+                    --file $FILENAME --address $SERVER_IP:$SERVER_PORT --iters $NB_FILES --txt-dir $TXT_DIR/ \
+                    --chunk-size $CLIENT_CHUNK_SIZE)
+                CODE=$?
+                ((TRY++))
+            done
+            #Add the result if successfull
+            if [[ $CODE -eq 0 ]]; then
+                ((NB_SUCCESS++))
+                echo "Success $NB_SUCCESS: adding the time iter time $ITER_TIME to time $TIME"
+                TIME=$(awk "BEGIN {print $TIME+$ITER_TIME; exit}")
+            fi
+            ((ITER++))
+        done
+        #Calculate the average time
+        if [[ $NB_SUCCESS -gt 0 ]]; then
+            echo "Success tries: $NB_SUCCESS. Divide time $TIME by it"
+            TIME=$(awk "BEGIN {print $TIME/$NB_SUCCESS; exit}")
+            echo "time: $TIME server_chunk_size: $SERVER_CHUNK_SIZE client_chunk_size: $CLIENT_CHUNK_SIZE"
+            echo "time: $TIME server_chunk_size: $SERVER_CHUNK_SIZE client_chunk_size: $CLIENT_CHUNK_SIZE" >>$RESULTS_FN
+        fi
+        # clean up downloaded files
+        rm -f $TXT_DIR/*
+        CLIENT_CHUNK_SIZE=$(($CLIENT_CHUNK_SIZE * 2))
+    done
+done
+$DIRNAME/icps_stop.sh >/dev/null
